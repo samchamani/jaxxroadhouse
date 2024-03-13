d5e2e828ca6c8f20a53452c4f8d37a2ab6d9a787
Sam Chamani <sam.chamani@live.de>
2024-03-13 09:40:51 +0100

    data struct for products

---
+++ src/data.ts
@@ -0,0 +1,29 @@
+export const PRODUCTS = [
+    {
+        name: "DIE BRATPFANNE",
+        description: "„Die Bratpfanne“ ist mehr als nur ein Küchenutensil – es ist ein Symbol für handwerkliche Perfektion und ästhetische Eleganz. Hergestellt aus feinstem Gusseisen und veredelt mit einem handgefertigten Holzgriff, repräsentiert sie eine nahtlose Verbindung von Funktionalität und Stil.",
+        price: 129.00,
+        imgUrls: ["https://jaxx-roadhouse.de/wp-content/uploads/2024/01/01.webp"],
+        rating: 4.5,
+        rating_count: 123,
+        options: [
+            {
+                label: "Seasoning",
+                values: [
+                    { name: "ohne Seasoning", addedCosts: 0 },
+                    { name: "mit Seasoning", addedCosts: 10 },
+                ]
+            },
+            {
+                label: "Holzgriff",
+                values: [
+                    { name: "Walnussholz", addedCosts: 0 },
+                    { name: "Rosenholz", addedCosts: 0 },
+                ]
+            }
+        ]
+
+    },
+
+    
+]
